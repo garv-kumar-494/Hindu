52ffa04b08539436c0573d7e717da890ff052741
Garv Kumar <garvgithub18@gmail.com>
2026-02-06 17:14:24 +0530

    nowwwwwwwwwwww

---
+++ user-membership.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+
+// 🔹 Schema
+const memberSchema = new mongoose.Schema({
+  name: String,
+  phone: String,
+  email: String,
+  password: String,
+  temple: String,
+   membershipStatus: {
+    type: String,
+    default: "locked"
+    } // 🔒 by default locked
+});
+
+// 🔹 Model
+const Member = mongoose.model("Member", memberSchema);
+
+
+
+
+// 🔍 CHECK USER MEMBERSHIP (USER SIDE)
+router.get("/check-membership", async (req, res) => {
+  const { email } = req.query;
+
+  try {
+    const member = await Member.findOne({ email });
+
+    if (!member) {
+      return res.json({ membershipStatus: "locked" });
+    }
+
+    res.json({
+      membershipStatus: member.membershipStatus
+    });
+
+  } catch (err) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+module.exports = router;
